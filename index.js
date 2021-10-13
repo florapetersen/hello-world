63e37013669d3e836be99d676a0a593524a8bd38
florapetersen <floramarina@comcast.net>
2021-10-13 05:40:49 -0700

    create route handler

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const port = process.env.PORT || "8000";
 /**
  * Routes Definitions
  */
-
+app.get("/", (req, res) => {
+    res.status(200).send("WHATABYTE: Food For Devs");
+});
 /**
  * Server Activation
  */
